bf652fdcff0964e1ae6d8c9503b54674dc72a408
MMMUFFINS <mmmuffins@thebakary.moe>
2018-01-04 14:45:06 +0000

    can delete old messages now

--- src/index.js
+++ src/index.js
@@ -16,18 +16,49 @@ client.on('ready', () => {
 client.on('message', message => {
     // ignore bots, including itself! THIS IS IMPORTANT
     if (message.author.bot) return;
-    
+
     let reply;
-    // send back the message and any associated properties lmao
-    let canManageMessages = message.member.hasPermission('MANAGE_MESSAGES');
-    if (canManageMessages) {
-        reply = 'You can manage messages!';
+
+    if (message.content === '!deletethis') {
+        let currentChannel = message.channel;
+
+        currentChannel.fetchMessages()
+        .then(messages => {
+            return currentChannel.bulkDelete(messages, true);
+        })
+        .then(deletedMessages => {
+            message.channel.send('I deleted everything! Tehe~ :heart:');
+        })
+        .catch(err => {
+            message.channel.send('error! ' + err.message);
+        })
     }
     else {
-        reply = 'You can\'t manage messages!';
+        // message.channel.send('uh huh...');
+        return;
     }
-    console.log(reply);
-    message.channel.send(reply);
+    
+    
+    // send back the message and any associated properties lmao
+    // let canManageMessages = message.member.hasPermission('MANAGE_MESSAGES');
+    // if (canManageMessages) {
+    //     reply = 'You can manage messages!';
+    // }
+    // else {
+    //     reply = 'You can\'t manage messages!';
+    // }
+    
+
+    
+
+    // message.delete(2000)
+    // .then((console.log('deleted message: ' + message.content)))
+    // .catch((err) => console.log(err))
+
+    // // reply = message.channel.messages.array();
+    // reply = Math.random();
+
+    
 });
 
 // program start
