99464f360e8cce7e3de398a4ecb76af86620570f
MMMUFFINS <mmmuffins@thebakary.moe>
2018-01-07 04:15:25 +0000

    deletes messages recursively

---
+++ src/includes/deletbot.js
@@ -0,0 +1,107 @@
+'use strict';
+
+module.exports = (() => {
+    class Deletbot {
+        constructor () {
+            this.deletMode = false;
+        }
+    
+        handleMessage (message) {
+            if (message.member && message.content === '!delet') {
+                let canManageMessages = message.member.hasPermission('MANAGE_MESSAGES');
+
+                if (canManageMessages) {
+                    if (message.content === '!delet' && this.deletMode === false) {
+                        this.deletMode = true;
+                        let currentChannel = message.channel;
+                        
+                        let continuousDelet = () => {
+                            console.log('continuousDelet')
+                            if (this.deletMode === true) {
+                                console.log('still in delet mode')
+                                return new Promise((resolve, reject) => {
+                                    currentChannel.fetchMessages()
+                                    .then(messages => {
+                                        let msgArray = messages.array();
+                                        let msgsForDeletion = [];
+
+                                        for (let i = 0; i < msgArray.length; i++) {
+                                            if (!msgArray[i].pinned) msgsForDeletion.unshift(msgArray[i]);
+                                        }
+                            
+                                        if (msgsForDeletion.length >= 2) {
+                                            return currentChannel.bulkDelete(msgsForDeletion, false)
+                                            .then(msgs => {
+                                                return Promise.resolve();
+                                            })
+                                            .catch(err => {
+                                                return Promise.reject();
+                                            });
+                                        }
+                            
+                                        else if (msgsForDeletion.length === 1) {
+                                            return msgsForDeletion[0].delete()
+                                            .then(msg => {
+                                                return Promise.resolve();
+                                            })
+                                            .catch(err => {
+                                                return Promise.reject(err);
+                                            });
+                                        }
+
+                                        else {
+                                            // done, no more
+                                            this.deletMode = false;
+                                            return Promise.resolve();
+                                        }
+                                    })
+                                    .then(() => {
+                                        console.log('going to call continuousDelet again')
+                                        return continuousDelet();   // will only return a resolved promise once 
+                                    })
+                                    .then(() => {
+                                        return resolve();
+                                    })
+                                    .catch(err => {
+                                        return reject(err);
+                                    });
+                                    
+                                })
+                            }
+                            else {
+                                console.log('exiting delet mode')
+                                return Promise.resolve();
+                            }
+                        };
+
+                        // start recursion
+                        continuousDelet()
+                        .then(() => {
+                            console.log('I deleted everything')
+                        })
+                        .catch(err => {
+                            message.channel.send('Error! ' + err.message);
+                        });
+                    }
+                    else {
+                        message.channel.send(
+                            'fuck off you don\'t have permissions to manage messages here.', 
+                            {
+                                reply: message.author
+                            }
+                        );
+                    }
+                  
+                }
+            }
+    
+            
+        }
+    }
+    
+    let deletbot = new Deletbot();
+
+    return deletbot;
+})();
+
+
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let config = JSON.parse(fs.readFileSync('./config.json'));
 let discordToken = config.secrets.discord;
 
 const client = new Discord.Client();
+const deletbot = require('./includes/deletbot.js');
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -17,47 +18,8 @@ client.on('message', message => {
     // ignore bots, including itself! THIS IS IMPORTANT
     if (message.author.bot) return;
 
-    let reply;
-
+    deletbot.handleMessage(message);
     // message.member is undefined for PMs
-    if (message.member) {
-        let canManageMessages = message.member.hasPermission('MANAGE_MESSAGES');
-
-        if (message.content === '!delet') {
-            if (canManageMessages) {
-                let currentChannel = message.channel;
-    
-                currentChannel.fetchMessages()
-                .then(messages => {
-                    let msgArray = messages.array();
-                    let msgsForDeletion = [];
-                    // ignore the first message
-                    for (let i = 0; i < msgArray.length; i++) {
-                        if (!msgArray[i].pinned) msgsForDeletion.unshift(msgArray[i]);
-                    }
-    
-                    if (msgsForDeletion.length >= 2) {
-                        return currentChannel.bulkDelete(msgsForDeletion, false);
-                    }
-    
-                    else {
-                        return msgsForDeletion[0].delete();
-                    }
-                })
-                .catch(err => {
-                    message.channel.send('error! ' + err.message);
-                });
-            }
-            else {
-                message.channel.send(
-                    'fuck off you don\'t have permissions to manage messages here.', 
-                    {
-                        reply: message.author
-                    }
-                );
-            }
-        }
-    }
 
 });
 
